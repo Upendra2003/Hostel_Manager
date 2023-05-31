caed22bc8307d426d202f2fc1adbdd0cb76dfdc6
Upendra2003 <upendrakarimi2003@gmail.com>
2023-05-31 22:48:55 +0530

    modified

--- server.py
+++ server.py
@@ -2,9 +2,13 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("signin.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 if __name__=="__main__":
     app.run(debug=True)
