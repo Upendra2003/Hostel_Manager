b2d66fffa9dbf680104692b478c8f57dafccadd3
Upendra2003 <upendrakarimi2003@gmail.com>
2023-06-03 22:06:15 +0530

    created backend for posting notice and getting notice

--- server.py
+++ server.py
@@ -9,13 +9,11 @@ client=pymongo.MongoClient('mongodb://localhost:27017')
 db=client['HSManager']
 collections=db['registrations']
 ad_collections=db['admin_reg']
+notice=db['notices']
 
-@app.route("/<email>")
-def home(email):
-    if email[0]=='b':
-        return render_template("student.html")
-    elif email[0]=='m':
-        return render_template("admin.html")
+@app.route("/")
+def home():
+    return "<h1> home </h1>"
 
 @app.route("/login",methods=['POST','GET'])
 def login():
@@ -26,14 +24,17 @@ def login():
         }
         if user['email'][0]=='b':
             find_user=collections.find_one({"email":user['email']})
+            if check_password_hash(find_user['password'],user['password']):
+                get_notice=notice.find()
+                return render_template("student.html",get_notice=get_notice,user_name=find_user['name'])
+            else:
+                return '404'
         elif user['email'][0]=='m':
             find_user=ad_collections.find_one({"email":user['email']})
-        print(find_user)
-        if check_password_hash(find_user['password'],user['password']):
-            return redirect(url_for('home',email=user['email']))
-        else:
-            return '404'
-
+            if check_password_hash(find_user['password'],user['password']):
+                return render_template("admin.html")
+            else:
+                return '404'
     return render_template("login.html")
 
 @app.route("/register",methods=['POST','GET'])
@@ -57,6 +58,36 @@ def register():
             ad_collections.insert_one(new_user)
             return redirect(url_for('home',email=new_user['email']))
     return render_template("register.html")
+
+@app.route("/post_notice",methods=['POST','GET'])
+def post_notice():
+    new_notice={
+        "notice":request.form.get('post_notice')
+    }
+    notice.insert_one(new_notice)
+    return redirect(url_for('home'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
