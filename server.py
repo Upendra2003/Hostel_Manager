c71ee50f404503e0bb3386c86c90274452a7fc15
Upendra2003 <upendrakarimi2003@gmail.com>
2023-06-05 02:08:00 +0530

    completed dashboard backend and css

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request,redirect,url_for
 import pymongo
 from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aaec0770c47c1563dfb147b3bdb14072'
@@ -13,7 +14,12 @@ notice=db['notices']
 
 @app.route("/")
 def home():
-    return "<h1> home </h1>"
+    return render_template("login.html")
+
+@app.route("/<name>")
+def home_name(name):
+    get_notice=notice.find()
+    return render_template("admin_dashboard.html",user_name=name,get_notice=get_notice)
 
 @app.route("/login",methods=['POST','GET'])
 def login():
@@ -22,17 +28,17 @@ def login():
             "email":request.form.get('email'),
             "password":request.form.get('password')
         }
+        get_notice=notice.find()
         if user['email'][0]=='b':
             find_user=collections.find_one({"email":user['email']})
             if check_password_hash(find_user['password'],user['password']):
-                get_notice=notice.find()
-                return render_template("student.html",get_notice=get_notice,user_name=find_user['name'])
+                return render_template("student_dashboard.html",get_notice=get_notice,user_name=find_user['name'])
             else:
                 return '404'
         elif user['email'][0]=='m':
             find_user=ad_collections.find_one({"email":user['email']})
             if check_password_hash(find_user['password'],user['password']):
-                return render_template("admin.html")
+                return render_template("admin_dashboard.html",get_notice=get_notice,user_name=find_user['name'],user_email=find_user['email'])
             else:
                 return '404'
     return render_template("login.html")
@@ -59,13 +65,19 @@ def register():
             return redirect(url_for('home',email=new_user['email']))
     return render_template("register.html")
 
-@app.route("/post_notice",methods=['POST','GET'])
-def post_notice():
+@app.route("/post/<name>")
+def post(name):
+    return render_template("post.html",user_name=name)
+
+@app.route("/post_notice/<name>",methods=['POST','GET'])
+def post_notice(name):
+    current_date = datetime.date.today().strftime('%d-%m-%Y')
     new_notice={
-        "notice":request.form.get('post_notice')
+        "notice":request.form.get('post_notice'),
+        "date":str(current_date)
     }
     notice.insert_one(new_notice)
-    return redirect(url_for('home'))
+    return redirect(url_for('home_name',name=name))
 
 
 
